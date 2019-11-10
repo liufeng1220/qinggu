5f53d1bbd01879a78cb050afa10e0eaff8efc341
rafeily <473342599@qq.com>
2019-11-10 22:39:27 +0800

    根节点提交

--- qinggu-microservice-edu/src/main/java/co/liufeng/edu/service/impl/SubjectServiceImpl.java
+++ qinggu-microservice-edu/src/main/java/co/liufeng/edu/service/impl/SubjectServiceImpl.java
@@ -1,17 +1,21 @@
 package co.liufeng.edu.service.impl;
 
-import co.liufeng.edu.dto.ExcelData;
+import co.liufeng.common.util.ExcelImportUtil;
 import co.liufeng.edu.entity.Subject;
-import co.liufeng.edu.listener.UploadDataListener;
 import co.liufeng.edu.mapper.SubjectMapper;
 import co.liufeng.edu.service.SubjectService;
-import com.alibaba.excel.EasyExcel;
+import co.liufeng.edu.vo.SubjectVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -34,11 +38,118 @@ public class SubjectServiceImpl extends ServiceImpl<SubjectMapper, Subject> impl
      * @return
      * @throws Exception
      */
+
     @Override
     public List<String> batchImport(MultipartFile file) throws Exception {
-        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        //错误消息列表
+        List<String> errorMsg = new ArrayList<>();
+        //创建工具类对象
+        ExcelImportUtil excelHSSFUtil = new ExcelImportUtil(file.getInputStream());
+        //获取工作表
+        Sheet sheet = excelHSSFUtil.getSheet();
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        if (rowCount <= 1) {
+            errorMsg.add("请填写数据");
+            return errorMsg;
+        }
+        for (int rowNum = 1; rowNum < rowCount; rowNum++) {
+            Row rowData = sheet.getRow(rowNum);
+            if (rowData != null) {// 行不为空
+                //获取一级分类
+                String levelOneValue = "";
+                Cell levelOneCell = rowData.getCell(0);
+                if (levelOneCell != null) {
+                    levelOneValue = excelHSSFUtil.getCellValue(levelOneCell).trim();
+                    if (StringUtils.isEmpty(levelOneValue)) {
+                        errorMsg.add("第" + rowNum + "行一级分类为空");
+                        continue;
+                    }
+                }
+                //判断一级分类是否重复
+                Subject subject = this.getByTitle(levelOneValue);
+                String parentId = null;
+                if (subject == null) {
+                    //将一级分类存入数据库
+                    Subject subjectLevelOne = new Subject();
+                    subjectLevelOne.setTitle(levelOneValue);
+                    subjectLevelOne.setSort(rowNum);
+                    baseMapper.insert(subjectLevelOne);//添加
+                    parentId = subjectLevelOne.getId();
+                } else {
+                    parentId = subject.getId();
+                }
+                //获取二级分类
+                String levelTwoValue = "";
+                Cell levelTwoCell = rowData.getCell(1);
+                if (levelTwoCell != null) {
+                    levelTwoValue = excelHSSFUtil.getCellValue(levelTwoCell).trim();
+                    if (StringUtils.isEmpty(levelTwoValue)) {
+                        errorMsg.add("第" + rowNum + "行二级分类为空");
+                        continue;
+                    }
+                }
 
-        EasyExcel.read(file.getInputStream(), ExcelData.class, new UploadDataListener(subjectMapper)).sheet().doRead();
-        return null;
+                //判断二级分类是否重复
+                Subject subjectSub = this.getSubByTitle(levelTwoValue, parentId);
+                Subject subjectLevelTwo = null;
+                if (subjectSub == null) {
+                    //将二级分类存入数据库
+                    subjectLevelTwo = new Subject();
+                    subjectLevelTwo.setTitle(levelTwoValue);
+                    subjectLevelTwo.setParentId(parentId);
+                    subjectLevelTwo.setSort(rowNum);
+                    baseMapper.insert(subjectLevelTwo);//添加
+                }
+            }
+        }
+        return errorMsg;
+    }
+
+    /**
+     * 树形查询
+     * @return
+     */
+    @Override
+    public List<SubjectVo> listSubjectVo() {
+        //查询所有根节点数据
+        List<SubjectVo> subjectVos = new ArrayList<SubjectVo>();
+        QueryWrapper<Subject> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id", "0");
+        List<Subject> subjects = baseMapper.selectList(wrapper);
+        for (int i = 0; i < subjects.size(); i++) {
+            QueryWrapper<Subject> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("parent_id", subjects.get(i).getId());
+            List<Subject> subjects1 = baseMapper.selectList(queryWrapper);
+
+            subjectVos.add(new SubjectVo(subjects.get(i).getId(), subjects.get(i).getTitle(),subjects1));
+        }
+        return subjectVos;
+
+    }
+
+    /**
+     * 根据分类名称查询这个一级分类中否存在
+     *
+     * @param title
+     * @return
+     */
+    private Subject getByTitle(String title) {
+        QueryWrapper<Subject> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("title", title);
+        queryWrapper.eq("parent_id", "0");//一级分类
+        return baseMapper.selectOne(queryWrapper);
+    }
+
+    /**
+     * 根据分类名称和父id查询这个二级分类中否存在
+     *
+     * @param title
+     * @return
+     */
+    private Subject getSubByTitle(String title, String parentId) {
+        QueryWrapper<Subject> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("title", title);
+        queryWrapper.eq("parent_id", parentId);
+        return baseMapper.selectOne(queryWrapper);
     }
 }
