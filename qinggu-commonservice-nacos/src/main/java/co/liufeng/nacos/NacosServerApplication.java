18a0f633673681f40d95b6a3096e85fe682ebec1
rafeily <473342599@qq.com>
2019-11-13 15:35:03 +0800

    存档，创建用户中心完成

--- qinggu-commonservice-nacos/src/main/java/co/liufeng/nacos/NacosServerApplication.java
+++ qinggu-commonservice-nacos/src/main/java/co/liufeng/nacos/NacosServerApplication.java
@@ -1,19 +1,17 @@
-package co.liufeng.edu;
+package co.liufeng.nacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author rafeily
- * @date 2019/10/30 16:26
+ * @date 2019/11/12 17:55
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"co.liufeng.edu","co.liufeng.common"})
-public class EduApplication {
+public class NacosServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EduApplication.class,args);
+        SpringApplication.run(NacosServerApplication.class,args);
     }
 }
